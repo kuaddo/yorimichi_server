0593dd4b51dbfc2fd91b16090a4c9cac2ecdcb87
y-kisse <15102288+y-kisse@users.noreply.github.com>
2018-10-25 11:07:08 +0900

    [update] add http status code

--- v1/views/index.py
+++ v1/views/index.py
@@ -4,4 +4,4 @@ app = Blueprint('index', __name__, url_prefix='/v1')
 
 @app.route('/')
 def index():
-  return make_response(jsonify({'message': 'This is v1 API root'}))
+  return make_response(jsonify({'message': 'This is v1 API root'}), 200)
