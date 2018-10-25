07cd6ec9f34f984bb84d44b540a2076a36a6805b
y-kisse <15102288+y-kisse@users.noreply.github.com>
2018-10-25 11:07:36 +0900

    [add] query to fetch posts by uuid

--- models/users.py
+++ models/users.py
@@ -23,6 +23,25 @@ def create_user():
 
   return None
 
-def get_user(uuid):
+def get_user_by_uuid(uuid):
   stmt = 'SELECT * FROM users WHERE uuid = \'{}\''.format(uuid)
   return query(stmt)
+
+def get_posts_by_uuid(uuid):
+  stmt = '''
+    SELECT  p.id AS id,
+            p.user_id AS user_id,
+            p.place_uid AS place_uid,
+            "gs://yorimichi_posts" AS bucket,
+            p.image_name AS image_name,
+            DATE_FORMAT(p.created_at, '%Y-%m-%d %H:%i:%S') AS created_at,
+            DATE_FORMAT(p.updated_at, '%Y-%m-%d %H:%i:%S') AS updated_at
+    FROM    posts AS p
+      INNER JOIN users AS u ON u.id = p.user_id
+    WHERE   u.uuid = "{}"
+      AND   u.is_valid = 1
+      AND   p.is_valid = 1
+  '''.format(uuid)
+
+  res = query(stmt)
+  return res
