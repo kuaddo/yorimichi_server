c3ba77ab1f315fc9b331ce3d761bb7ff93dc3ec3
y-kisse <15102288+y-kisse@users.noreply.github.com>
2018-10-29 23:48:52 +0900

    [add] API to give user points

--- v1/views/users.py
+++ v1/views/users.py
@@ -1,4 +1,5 @@
-from models.users import create_user, get_posts_by_uuid
+from models.users import create_user, get_posts_by_uuid, add_point
+from models.point_history import create_point_history
 from utils.token import check_api_token
 
 from flask import Blueprint, jsonify, make_response, request
@@ -34,3 +35,26 @@ def posts(uuid):
     return make_response(jsonify({'message': 'Request Completed.', 'posts_array': res}), 200)
   else:
     return '', 204
+
+@app.route('<uuid>/points/', methods=['POST'])
+def points(uuid):
+  # API Token exist?
+  if 'X_API_Token' not in request.headers:
+    return make_response(jsonify({'message': 'API token not found'}), 400)
+  # API Token Correct?
+  if not check_api_token(request.headers['X_API_Token']):
+    return make_response(jsonify({'message': 'API token is not correct'}), 400)
+
+  # check Content-Type header
+  if 'application/json' not in  request.headers['Content-Type'].split(';'):
+    return make_response(jsonify({'message': 'error'}), 400)
+
+  
+  user_records = get_user_by_uuid(uuid)
+  if len(user_records) == 0:
+    return make_response(jsonify({'message': 'No user found on specified uuid'}), 400)
+
+  add_point(uuid, json['point'])
+  create_point_history(user_records[0]['user_id'], json['point'])
+
+  return make_response(jsonify({'message': 'Points successfuly gave to user.'}), 200)
