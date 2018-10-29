de55816056e436ebe4e675cfe1f3c5d4a945f1cf
y-kisse <15102288+y-kisse@users.noreply.github.com>
2018-10-29 13:01:18 +0900

    [update] /v1/users/ require API Key

--- v1/views/users.py
+++ v1/views/users.py
@@ -1,4 +1,5 @@
 from models.users import create_user, get_posts_by_uuid
+from utils.token import check_api_token
 
 from flask import Blueprint, jsonify, make_response, request
 
@@ -6,6 +7,13 @@ app = Blueprint('users', __name__, url_prefix='/v1/users')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+  # API Token exist?
+  if 'X_API_Token' not in request.headers:
+    return make_response(jsonify({'message': 'API token not found'}), 400)
+  # API Token Correct?
+  if not check_api_token(request.headers['X_API_Token']):
+    return make_response(jsonify({'message': 'API token is not correct'}), 400)
+  # API Key Corrected, begin create user.
   if request.method == 'POST':
     uuid = create_user()
     return make_response(jsonify({'message': 'Create User.', 'uuid': uuid}))
