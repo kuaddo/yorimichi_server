431e68b4d1ce22d13ad5b4d24efc440477edadbb
y-kisse <15102288+y-kisse@users.noreply.github.com>
2018-11-02 23:22:15 +0900

    [update] bytype, bykeyword wrapper

--- utils/api_wrapper.py
+++ utils/api_wrapper.py
@@ -1,11 +1,48 @@
 import os
 import urllib.request
 
-def bykeyword(location, radius, keyword):
-  pass
+baseurl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?{}"
 
-def bytype(location, radius, type):
-  pass
+def bykeyword(location, radius, keyword):
+  params = {
+    'language': 'ja',
+    'location': location,
+    'radius': radius,
+    'keyword': keyword,
+    'key': os.environ['PLACE_API_KEY']
+  }
+
+  url = baseurl.format(urllib.parse.urlencode(params)) + '&opennow'
+  print(url)
+  req = urllib.request.Request(url)
+  try:
+    with urllib.request.urlopen(req) as res:
+      body = res.read().decode('utf-8')
+      return body
+  except urllib.error.HTTPError as err:
+    print(err)
+  except urllib.error.URLError as err:
+    print(err)
+
+def bytype(location, radius, type_value):
+  params = {
+    'language': 'ja',
+    'location': location,
+    'radius': radius,
+    'type': type_value,
+    'key': os.environ['PLACE_API_KEY']
+  }
+
+  url = baseurl.format(urllib.parse.urlencode(params)) + '&opennow'
+  req = urllib.request.Request(url)
+  try:
+    with urllib.request.urlopen(req) as res:
+      body = res.read().decode('utf-8')
+      return body
+  except urllib.error.HTTPError as err:
+    print(err)
+  except urllib.error.URLError as err:
+    print(err)
 
 def nextpage(nextToken):
   pass
